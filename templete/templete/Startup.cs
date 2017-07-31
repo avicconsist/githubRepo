f9c51c03a338559dda63f75fe1db215595912ecd
avic <avic.consist@gmail.com>
2017-07-31 12:33:32 +0300

    add project "templete"

---
+++ templete/templete/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(templete.Startup))]
+namespace templete
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
