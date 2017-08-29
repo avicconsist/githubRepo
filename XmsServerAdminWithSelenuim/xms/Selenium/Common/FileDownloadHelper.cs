84040c13fde5e2e9b402ee9f35c3c38acd7af625
avic <avic.consist@gmail.com>
2017-08-29 19:32:32 +0300

    add tests

--- XmsServerAdminWithSelenuim/xms/Selenium/Common/FileDownloadHelper.cs
+++ XmsServerAdminWithSelenuim/xms/Selenium/Common/FileDownloadHelper.cs
@@ -1,14 +1,12 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
-using System.Collections.Generic;
-using Selenium.Models;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading;
 using System.IO;
 using System;
-
-using OpenQA.Selenium.Interactions;
 using Microsoft.Office.Interop.Excel;
+
 
 namespace Selenium.Common
 {
@@ -153,8 +151,9 @@ namespace Selenium.Common
 
             var cardcontent = container[0].FindElement(By.ClassName("card-content"));
             var cardname = "d" + container[0].FindElement(By.ClassName("card-name")).Text;
-            string path = Environment.GetEnvironmentVariable("USERPROFILE") + "\\Downloads\\" + cardname + "update.xlsm";
-            var cardbuttons = cardcontent.FindElements(By.ClassName("btn-group"));
+            string path = Environment.GetEnvironmentVariable("USERPROFILE") + "\\Downloads\\" + cardname + ".xlsm";
+
+            path = UpdateReportExcel(cardname);
 
             cardcontent.FindElement(By.Id("unique1")).SendKeys(path);
 
@@ -167,26 +166,95 @@ namespace Selenium.Common
             WaitHelper.Wait.Timeout = TimeSpan.FromSeconds(10);
             var validationmessages = SeleniumDriver.driver.FindElement(By.Id("validation-messages-modal"));
             validationmessages.FindElement(By.ClassName("button")).Click();
-
-           var status= cardcontent.FindElement(By.ClassName("card-xbrl-status"));
+            Thread.Sleep(500);
+            var status = cardcontent.FindElement(By.ClassName("card-xbrl-status"));
             if (!status.Text.Contains("דיווח שגוי"))
             {
                 Assert.Fail("card-xbrl-status not change after excel errors ");
             }
 
-            var cardlastupdatedate= cardcontent.FindElement(By.ClassName("card-last-update-date"));
+            var cardlastupdatedate = cardcontent.FindElement(By.ClassName("card-last-update-date"));
 
-            if (cardlastupdatedate.Text!= DateTime.Now.ToString("dd/MM/yyyy"))
+            if (cardlastupdatedate.Text != DateTime.Now.ToString("dd/MM/yyyy"))
             {
                 Assert.Fail("card-last-update-date not updated after update");
             }
             var cardlastupdatetime = cardcontent.FindElement(By.ClassName("card-last-update-time"));
 
-             //this not working....
-             if (!cardlastupdatetime.Text.Contains(DateTime.Now.TimeOfDay.ToString("HH:mm:ss")))
-             {
-                 Assert.Fail("card-last-update-date not updated after update");
-             }
+            path = Environment.GetEnvironmentVariable("USERPROFILE") + "\\Downloads\\" + cardname + ".xlsm";
+
+            cardcontent.FindElement(By.Id("unique1")).SendKeys(path);
+            Thread.Sleep(500);
+            WaitHelper.Wait.Timeout = TimeSpan.FromSeconds(20);
+            WaitHelper.Wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.ClassName("loading-message")));
+            WaitHelper.Wait.Timeout = TimeSpan.FromSeconds(10);
+
+            WaitHelper.Wait.Until(ExpectedConditions.ElementToBeClickable(By.ClassName("card-xbrl-status")));
+
+            status = cardcontent.FindElement(By.ClassName("card-xbrl-status"));
+            if (!status.Text.Contains("תקין"))
+            {
+                Assert.Fail("card-xbrl-status not change after excel errors ");
+            }
+            path = Environment.GetEnvironmentVariable("USERPROFILE") + "\\Downloads\\testing.xlsx";
+
+            cardcontent.FindElement(By.Id("unique1")).SendKeys(path);
+
+            WaitHelper.Wait.Timeout = TimeSpan.FromSeconds(20);
+            WaitHelper.Wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.ClassName("loading-message")));
+
+            WaitHelper.Wait.Until(ExpectedConditions.ElementToBeClickable(By.ClassName("xms-reports-filter-period-type")));
+
+            WaitHelper.Wait.Timeout = TimeSpan.FromSeconds(3);
+            var dialog = WaitHelper.WaitToConfrimWindow();
+            WaitHelper.Wait.Timeout = TimeSpan.FromSeconds(10);
+
+            var btnOk = dialog.FindElement(By.ClassName("swal2-confirm"));
+            btnOk.Click();
+            Thread.Sleep(500);
+            var lastupdates = cardcontent.FindElement(By.ClassName("card-last-update-log"));
+            lastupdates.Click();
+            Thread.Sleep(500);
+            WaitHelper.Wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.ClassName("loading-message")));
+            WaitHelper.Wait.Until(ExpectedConditions.ElementIsVisible(By.Id("log-modal")));
+            var logmodal = SeleniumDriver.driver.FindElement(By.Id("log-modal"));
+
+            var table = logmodal.FindElement(By.TagName("tbody"));
+            var rows = table.FindElements(By.TagName("tr"));
+
+            var td = rows[0].FindElements(By.TagName("td"));
+            if (td[0].Text != DateTime.Now.ToString("dd/MM/yyyy"))
+            {
+                Assert.Fail("testing  Last reports updates failed");
+            }
+
+            if (td[3].Text != "תקין")
+            {
+                Assert.Fail("testing  Last reports updates failed");
+            }
+
+            var td1 = rows[1].FindElements(By.TagName("td"));
+            if (td1[0].Text != DateTime.Now.ToString("dd/MM/yyyy"))
+            {
+                Assert.Fail("testing  Last reports updates failed");
+            }
+            if (td1[3].Text != "דיווח שגוי")
+            {
+                Assert.Fail("testing  Last reports updates failed");
+            }
+
+            var index = rows.Count - 1;
+            var name = container[0].FindElement(By.ClassName("card-name")).Text + "." + index;
+            var downloadexcel = td[4].FindElement(By.ClassName("download-xbrl-xls-button"));
+            downloadexcel.Click();
+            Thread.Sleep(500);
+            WaitHelper.Wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.ClassName("loading-message")));
+            if (!CheckFileDownloaded(name))
+            {
+                Assert.Fail("Can not download a version of a report from the last Updates window");
+            }
+
+            SeleniumDriver.driver.Navigate().Refresh();
         }
 
         public static void DeleteTemplet()
@@ -232,6 +300,34 @@ namespace Selenium.Common
             }
             AdminPageHelper.SwitchToMainPage();
             SeleniumDriver.driver.Navigate().Refresh();
+        }
+
+        public static string UpdateReportExcel(string cardname)
+        { 
+            var path = Environment.GetEnvironmentVariable("USERPROFILE") + "\\Downloads\\" + cardname + ".xlsm";
+
+            Application xlsmApp = new Application();
+            Workbook xlsm = xlsmApp.Workbooks.Open(path, ReadOnly: false);
+
+            Application xlApp = new Application();
+              
+            Workbook xlWorkBook = xlsm;
+
+            Worksheet xlWorkSheet = (Worksheet)xlWorkBook.Worksheets.get_Item(2);
+            xlWorkSheet.Activate(); 
+
+            xlApp.DefaultSheetDirection = (int)Constants.xlRTL;
+
+            xlWorkSheet.Cells[14, 4] = 9;
+             
+            path = Environment.GetEnvironmentVariable("USERPROFILE") + "\\Downloads\\" + cardname + "update.xlsm";
+
+            xlWorkBook.SaveAs(path);
+            xlWorkBook.Close();
+            xlApp.Quit();
+
+            return path;
+
         }
     }
 }
